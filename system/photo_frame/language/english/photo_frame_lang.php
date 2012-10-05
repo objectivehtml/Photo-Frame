0edc49947739fb66924f40b4c36ec27000d9ceca
Justin Kimbrell <justin@objectivehtml.com>
2012-10-05 10:52:59 -0400

    Updated lang file

--- system/photo_frame/language/english/photo_frame_lang.php
+++ system/photo_frame/language/english/photo_frame_lang.php
@@ -5,7 +5,7 @@ $lang	= array(
 	'photo_frame_module_name'			=> 'Photo Frame',
 	'photo_frame_module_description'	=> '',
 		
-	'photo_frame_button_text'			=> 'Upload Photo',
+	'photo_frame_button_text'			=> 'Upload',
 	'photo_frame_instructions'			=> 'Crop the photo by clicking and dragging on the photo.',
 	
 			
