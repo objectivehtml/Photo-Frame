9512c5a4435928e772ac8be774902774fd8835ab
Justin Kimbrell <justin@objectivehtml.com>
2012-10-31 23:57:22 -0400

    Increased version to 0.7.0

--- system/photo_frame/config/photo_frame_config.php
+++ system/photo_frame/config/photo_frame_config.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-$config['photo_frame_version'] = '0.6.0';
+$config['photo_frame_version'] = '0.7.0';
 
 if(!defined('PHOTO_FRAME_VERSION'))
 {
--- system/photo_frame/ft.photo_frame.php
+++ system/photo_frame/ft.photo_frame.php
@@ -7,8 +7,8 @@
  * @author		Justin Kimbrell
  * @copyright	Copyright (c) 2012, Objective HTML
  * @link 		http://www.objectivehtml.com/photo-frame
- * @version		0.6.0
- * @build		20121007
+ * @version		0.7.0
+ * @build		20121031
  */
 
 require 'config/photo_frame_config.php';
