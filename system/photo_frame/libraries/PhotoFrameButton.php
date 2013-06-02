65eabfc36ee470fc9066f7872b89848e0358f3ae
Justin Kimbrell <justin@objectivehtml.com>
2013-06-02 15:29:25 -0400

    Bug fixes

--- system/photo_frame/libraries/PhotoFrameButton.php
+++ system/photo_frame/libraries/PhotoFrameButton.php
@@ -10,51 +10,54 @@ if(!class_exists('ImageEditor'))
 	require_once(PATH_THIRD . 'photo_frame/libraries/ImageEditor.php');
 }
 
-abstract class PhotoFrameButton extends BaseClass {
-	
-	public $name;
-	
-	public $description;
-	
-	public $path;
-	
-	public $url;
-	
-	public $image = FALSE;
-	
-	public function __construct($params = array()) 
-	{
-		parent::__construct($params);
+if(!class_exists('PhotoFrameButton'))
+{
+	abstract class PhotoFrameButton extends BaseClass {
+		
+		public $name;
+		
+		public $description;
+		
+		public $path;
+		
+		public $url;
+		
+		public $image = FALSE;
+		
+		public function __construct($params = array()) 
+		{
+			parent::__construct($params);
+		}
+		
+		public function install() { }
+		
+		public function update($current = '') { }
+		
+		public function uninstall() { }
+		
+		public function modifyTables($table = array()) { return $table; }
+		
+		public function render($manipulation = array()) {}
+		
+		public function postSave($photo)
+		{
+			return $photo;
+		}
+		
+		public function startCrop($data = array())
+		{ 
+			return array(); 
+		}
+		
+		public function css()
+		{
+			return array();
+		}
+		
+		public function javascript()
+		{
+			return array();
+		}
+		
 	}
-	
-	public function install() { }
-	
-	public function update($current = '') { }
-	
-	public function uninstall() { }
-	
-	public function modifyTables($table = array()) { return $table; }
-	
-	public function render($manipulation = array()) {}
-	
-	public function postSave($photo)
-	{
-		return $photo;
-	}
-	
-	public function startCrop($data = array())
-	{ 
-		return array(); 
-	}
-	
-	public function css()
-	{
-		return array();
-	}
-	
-	public function javascript()
-	{
-		return array();
-	}
-	
 }
