27582d282326174f2af059dab73e35336509a213
Justin Kimbrell <justin@objectivehtml.com>
2013-06-02 13:24:43 -0400

    Updated version

--- system/photo_frame/config/photo_frame_config.php
+++ system/photo_frame/config/photo_frame_config.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-$config['photo_frame_version'] = '0.9.510';
+$config['photo_frame_version'] = '0.9.511';
 
 if(!defined('PHOTO_FRAME_VERSION'))
 {
