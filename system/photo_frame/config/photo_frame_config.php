67dde7951bc5fec5d24465eaac645abdb41726dd
Justin Kimbrell <justin@objectivehtml.com>
2013-09-05 10:08:43 -0400

    - (Feature) Updated config file so every config option can be overwritten with a personalized bootstrap file. This is good for working with multiple environments.

--- system/photo_frame/config/photo_frame_config.php
+++ system/photo_frame/config/photo_frame_config.php
@@ -1,27 +1,57 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-$config['photo_frame_version'] = '1.1.1';
+$config['photo_frame_version'] = '1.1.2';
 
 if(!defined('PHOTO_FRAME_VERSION'))
 {
 	define('PHOTO_FRAME_VERSION', $config['photo_frame_version']);
 }
 
-$config['photo_frame_directory_name']         = '_framed';
-$config['photo_frame_cache_directory']		  = '_cache';
-$config['photo_frame_default_size']           = 'framed';
-$config['photo_frame_original_size']          = 'original';
-$config['photo_frame_random_string_len']      = 8;
-$config['photo_frame_extra_dir_name']         = 'photo_frame_extras';
-$config['photo_frame_save_colors']            = 8;
-$config['photo_frame_save_color_granularity'] = 45;
+$config['photo_frame_directory_name']         = config_item('photo_frame_directory_name') ? 
+												config_item('photo_frame_directory_name') :
+												'_framed';
+
+$config['photo_frame_cache_directory']		  = config_item('photo_frame_cache_directory') ? 
+												config_item('photo_frame_cache_directory') :
+												'_cache';
+
+$config['photo_frame_default_size']		      = config_item('photo_frame_default_size') ? 
+												config_item('photo_frame_default_size') :
+												'framed';
+
+$config['photo_frame_original_size']		  = config_item('photo_frame_original_size') ? 
+												config_item('photo_frame_original_size') :
+												'original';
+
+$config['photo_frame_random_string_len']	  = config_item('photo_frame_random_string_len') ? 
+												config_item('photo_frame_random_string_len') :
+												8;
+
+$config['photo_frame_save_colors']		      = config_item('photo_frame_save_colors') ? 
+												config_item('photo_frame_save_colors') :
+												8;
+
+$config['photo_frame_save_color_granularity'] = config_item('photo_frame_save_color_granularity') ? 
+												config_item('photo_frame_save_color_granularity') :
+												45;
 
 // Dynamic caching for front-end use. (For Focal cropping)
 
-$config['photo_frame_front_end_cache_path']	  = FALSE;
-$config['photo_frame_front_end_cache_url']	  = '/assets/img/cache/';
-$config['photo_frame_front_end_cache_length'] = 0; //3600 * 24; // (in seconds)
+$config['photo_frame_front_end_cache_url']	  = config_item('photo_frame_front_end_cache_url') ? 
+												config_item('photo_frame_front_end_cache_url') : 
+												// Replace this value here if not using a modified bootstrap
+												FALSE;
 
+$config['photo_frame_front_end_cache_path']	  = config_item('photo_frame_front_end_cache_path') ?
+												config_item('photo_frame_front_end_cache_path') : 
+												// Replace this value here if not using a modified bootstrap
+												FALSE;
+
+$config['photo_frame_front_end_cache_length'] = config_item('photo_frame_front_end_cache_length') ?
+												config_item('photo_frame_front_end_cache_length') : 
+												// Replace this value here if not using a modified bootstrap
+												// Example: (3600 * 24 * 7); In seconds
+												0;
 /* 
  *	Define the ee() function introduced in EE 2.6
  */
