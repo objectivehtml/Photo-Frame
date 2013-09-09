43cbf52b765310b221f4579ed132413f2528f3f8
Justin Kimbrell <justin@objectivehtml.com>
2013-09-09 12:55:10 -0400

    Version bump

--- system/photo_frame/config/photo_frame_config.php
+++ system/photo_frame/config/photo_frame_config.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-$config['photo_frame_version'] = '1.2.0';
+$config['photo_frame_version'] = '1.2.1';
 
 if(!defined('PHOTO_FRAME_VERSION'))
 {
